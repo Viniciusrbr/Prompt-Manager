d4ac639df7bd67b7d216c17aaf69175dd4d68b65
viniciusrbr <viniciusgoes159@gmail.com>
2026-02-03 21:29:41 -0300

    refactor: update button styles and global CSS variables; remove example test

--- src/tests/example.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@/lib/test-utils';
-
-describe('Example Test Suite', () => {
-  it('should pass this example test', () => {
-    render(<div>Hello, World! </div>);
-
-    expect(screen.getByText('Hello, World!')).toBeInTheDocument();
-  });
-});
