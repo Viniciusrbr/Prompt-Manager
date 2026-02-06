92a301597c23b589f9749ae9ae610e1bcba76335
viniciusrbr <viniciusgoes159@gmail.com>
2026-02-05 21:19:20 -0300

    feat: update prisma to 7 and create db seed

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -4,15 +4,5 @@ import { SidebarContent } from './sidebar-content';
 export const Sidebar = async () => {
   const prompts = await prisma.prompt.findMany();
 
-  return (
-    <SidebarContent
-      prompts={[
-        {
-          id: '1',
-          title: 'Prompt 1',
-          content: 'Content 1',
-        },
-      ]}
-    />
-  );
+  return <SidebarContent prompts={prompts} />;
 };
