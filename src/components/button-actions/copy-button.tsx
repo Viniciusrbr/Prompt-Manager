e742186b45885fdc4eef6ca53a6b995b7d724d99
viniciusrbr <viniciusgoes159@gmail.com>
2026-03-22 18:17:10 -0300

    feat: adicionar framer-motion para animações nos componentes de interface

--- src/components/button-actions/copy-button.tsx
+++ src/components/button-actions/copy-button.tsx
@@ -4,6 +4,7 @@ import { Check, Copy } from 'lucide-react';
 import { useEffect, useRef, useState } from 'react';
 import { toast } from 'sonner';
 import { Button } from '../ui/button';
+import { motion } from 'motion/react';
 
 export type CopyButtonProps = {
   content: string;
@@ -56,9 +57,17 @@ export const CopyButton = ({ content }: CopyButtonProps) => {
       {isCopied ? (
         <Check className="w-4 h-4 text-green-400" />
       ) : (
-        <Copy className="w-4 h-" />
+        <Copy className="w-4 h-4" />
       )}
-      <span>{isCopied ? 'Copiado' : 'Copiar'}</span>
+      <motion.span
+        key={isCopied ? 'copiado' : 'copiar'}
+        initial={{ opacity: 0, x: 2 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, y: -2 }}
+        transition={{ duration: 0.2 }}
+      >
+        {isCopied ? 'Copiado' : 'Copiar'}
+      </motion.span>
     </Button>
   );
 };
